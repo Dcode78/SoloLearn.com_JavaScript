5b61ad562772bd823dc8ba985c79e973bc6de056
DamithaU <32234887+Dcode78@users.noreply.github.com>
2022-10-07 11:35:21 +0530

    testone

--- testone.js
+++ testone.js
@@ -1,18 +1,20 @@
 function main() {
     //take flight number and its status
-    // var flightNumber = console.readLine();
+    // var flightNumber = readLine();
     // var flightStatus = readLine();
 
     var flightNumber = 121;
     var flightStatus = "landed";
     
     console.log("test");
+    console.log(flightNumber);
 
     var flight1 = new flight(flightNumber, flightStatus);
     //assign a flight object to flight1 variable
     
     //output
-    console.log('The flight ' + flight1.flightNumber + ' is ' + flight1.flightStatus);
+    console.log('The flight ' + this.flightNumber + ' is ' + this.flightStatus);
+    ddsdsds;
     
 }
 
